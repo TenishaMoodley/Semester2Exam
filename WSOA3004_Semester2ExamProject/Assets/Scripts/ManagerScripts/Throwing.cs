54b9618511a022695cf1ccc71409cb31454ba3c9
TenishaMoodley <2105982@students.wits.ac.za>
2021-10-20 13:54:12 +0200

    changes to the portals, animations, character positions and textures

---
+++ WSOA3004_Semester2ExamProject/Assets/Scripts/ManagerScripts/Throwing.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwing : MonoBehaviour
+{
+    //public bool IsTouchingGround = false;
+
+    public GameObject Jammo;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        /*if (collision.gameObject.tag == "Ground")
+        {
+            Debug.Log(IsTouchingGround);
+            IsTouchingGround = true;
+        }*/
+
+        if (collision.gameObject == Jammo)                  //if barrel is being carried , then it becomes a child of Jammo
+        {
+            Jammo.transform.parent = transform;
+        }
+
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == Jammo)                  //if barrel is being carried , then it is no longer a child of Jammo
+        {
+            Jammo.transform.parent = null;
+        }
+
+
+    }
+}
