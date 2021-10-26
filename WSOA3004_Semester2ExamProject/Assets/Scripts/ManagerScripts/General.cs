f49d1e76c7f4373c42cfe8f2c431839e9a71b584
1874467LuyoloMbatha <1874467@students.wits.ac.za>
2021-10-26 12:41:00 +0200

    Changes to  barrel script

--- WSOA3004_Semester2ExamProject/Assets/Scripts/ManagerScripts/General.cs
+++ WSOA3004_Semester2ExamProject/Assets/Scripts/ManagerScripts/General.cs
@@ -28,4 +28,6 @@ public class General : MonoBehaviour
             Line_GO.SetActive(true);
         }
     }
+
+
 }
--- WSOA3004_Semester2ExamProject/Assets/Scripts/ManagerScripts/Throwing.cs
+++ WSOA3004_Semester2ExamProject/Assets/Scripts/ManagerScripts/Throwing.cs
@@ -8,13 +8,14 @@ public class Throwing : MonoBehaviour
     ParticleSystem explosion; 
     public GameObject Jammo;
 
-    public void OnTriggerEnter2D(Collider2D collision)
+
+  /*  public void OnTriggerEnter2D(Collider2D collision)
     {
         /*if (collision.gameObject.tag == "Ground")
         {
             Debug.Log(IsTouchingGround);
             IsTouchingGround = true;
-        }*/
+        }
 
         if (collision.gameObject == Jammo)                  //if barrel is being carried , then it becomes a child of Jammo
         {
@@ -28,15 +29,32 @@ public class Throwing : MonoBehaviour
         {
             Jammo.transform.parent = null;
         }
-    }
+    } */
 
     private void OnCollisionEnter(Collision collision)
     {
-       // if (collision.gameObject.tag == "wallBreak")
+        if (collision.gameObject.tag == "Jammo")
         {
+            this.GetComponent<Rigidbody>().mass = 1f;
+            
           //  explosion.Play();
           //  Destroy(collision.gameObject);
            
         }
+        else if (collision.gameObject.tag != "Jammo") 
+        {
+            this.GetComponent<Rigidbody>().mass = 1000f;
+
+        }
+
+        if (collision.gameObject.tag == "Connection")
+        {
+            this.GetComponent<Rigidbody>().mass = 1000f;
+
+        }
+
+
+
+
     }
 }
