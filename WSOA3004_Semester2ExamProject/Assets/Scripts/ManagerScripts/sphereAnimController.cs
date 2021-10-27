7a019a06f833d6c8e57d345196dcf40a045b1505
TenishaMoodley <2105982@students.wits.ac.za>
2021-10-27 14:28:30 +0200

    changes on the timer bars and timing and jammo's speed

--- WSOA3004_Semester2ExamProject/Assets/Scripts/ManagerScripts/sphereAnimController.cs
+++ WSOA3004_Semester2ExamProject/Assets/Scripts/ManagerScripts/sphereAnimController.cs
@@ -22,7 +22,7 @@ public class sphereAnimController : MonoBehaviour
 
     IEnumerator pAnim() 
     {
-        yield return new WaitForSeconds(58);
+        yield return new WaitForSeconds(50);
         anim.SetBool("isExpanding", true);
         Debug.Log("The wait is over");
        
