cc5529a2683ed4378a575bd91306ac10177b0508
1874467LuyoloMbatha <1874467@students.wits.ac.za>
2021-10-30 23:35:35 +0200

    Revert "Revert "Changes""
    This reverts commit 2fc998e1a63f82f2a62c131b169c30d0bdf68253.

--- WSOA3004_Semester2ExamProject/Assets/Scripts/ManagerScripts/pickUp2.cs
+++ WSOA3004_Semester2ExamProject/Assets/Scripts/ManagerScripts/pickUp2.cs
@@ -4,41 +4,20 @@ using UnityEngine;
 
 public class pickUp2 : MonoBehaviour
 {
+    
     public Transform destination;
 
     void Update()
     {
-
-        if (Input.GetKey(KeyCode.E)) 
-        {
-            GetComponent<Rigidbody>().useGravity = true;
-           
-            Debug.Log("E Pressed");
-        }
-
-        /*if (Input.GetKeyUp(KeyCode.E)) 
+        if (Input.GetKey(KeyCode.E))
         {
             this.transform.parent = null;
             GetComponent<Rigidbody>().useGravity = true;
-
-        }*/
-
-         
+        }
     }
 
-    /*private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Jammo") 
-        {
-            Debug.Log("contact");
-            if (Input.GetKey(KeyCode.E))
-            {
-                GetComponent<Rigidbody>().useGravity = false;
-                this.transform.position = destination.position;
-                this.transform.parent = GameObject.Find("pickedUp").transform;
-            }
-        }
-    }*/
+    
+    
 
     private void OnTriggerEnter(Collider collide)
     {
@@ -50,9 +29,6 @@ public class pickUp2 : MonoBehaviour
             GetComponent<Rigidbody>().useGravity = false;
             this.transform.position = destination.position;
             this.transform.parent = GameObject.Find("pickedUp").transform;
-
         }
     }
-
-    
 }
